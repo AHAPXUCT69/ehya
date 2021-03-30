b3b3d02d572d76cb01fb84005fbabbf7eec1238d
Slastenin Nikolay <slastenin13@gmail.com>
2021-03-30 21:38:03 +0300

    Create: created navigation

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,74 @@
 $(document).ready(function() {
-	// $('body').hide()
+  // Выпадающее меню
+  var menuLink = $('.nav__link');
+  menuLink.on('click', function() {
+    $(this).siblings('.nav__submenu').toggleClass('nav__submenu--visible');
+  });
+  var menuBottomLink = $('.nav-bottom__link');
+  menuBottomLink.on('click', function() {
+    $(this).siblings('.nav-bottom__submenu').toggleClass('nav-bottom__submenu--visible');
+  });
+  //  Маска для телефонов
+  $('.phone').mask('+7(000)-000-00-00');
+  // Menu Button
+  var menuButton = $('.header-top__burger');
+  menuButton.on('click', function () {
+    $('.nav-bottom').toggleClass('nav-bottom--visible')
+  });
+
+  // Modal
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  };
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  };
+
+  $(document).on('keydown', function(event) {
+          if (event.key == "Escape") {
+            closeModal(event);
+          }
+      });
+  var modalOverlay = $(".modal__overlay");
+  modalOverlay.on('click', closeModal);
+
+// Валидация форм
+  $('.form').each(function() {
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required:"Укажите свое имя",
+        minlength: "Слишком короткое"
+      },
+      phone: {
+        minlength: jQuery.validator.format("Номер введен некорректно"),
+        required: "Укажите номер телефона"
+      },
+      mail: {
+        required: "Укажите адрес вашей почты",
+        email: "Некорректный адрес"
+      },
+      email: {
+        required: "Укажите адрес вашей почты",
+        email: "Некорректный адрес"
+      },
+  },
+
+  });
+  })
+
 })
